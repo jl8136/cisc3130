dba3542bcfb21407e2b46aa83f512efe88290b6e
jl8136 <wasd218312@gmail.com>
2026-09-20 15:31:25 -0400

    hw3 stack

---
+++ homework3/Main.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(15);
+        stack.push(25);
+        stack.push(35);
+        stack.push(45);
+        stack.push(55);
+        System.out.println("Pushed 15, 25, 35, 45, 55 to the stack");
+        try {
+            System.out.println("Top item: " + stack.peek()); // 55
+            System.out.println("Removing: " + stack.pop()); // 55
+            System.out.println("Removing: " + stack.pop()); // 45
+            System.out.println("New Top:  " + stack.peek()); // 35
+            System.out.println("Is Empty?: " + stack.isEmpty()); // false
+
+
+
+        }
+        catch(Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
+
+class Stack {
+    private final ArrayList<Integer> stack;
+
+    public Stack() {
+        this.stack = new ArrayList<>();
+    }
+    public void push(int value) {
+        stack.add(value);
+    }
+
+    public int pop() throws Exception {
+       if (this.isEmpty()) {
+           throw new Exception("Stack is empty");
+       }
+        return stack.remove(stack.size() - 1);
+    }
+
+    public int peek() throws Exception {
+        if (this.isEmpty()) {
+            throw new Exception("Stack is empty");
+        }
+        return stack.get(stack.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
---
+++ homework3/readme.md
@@ -0,0 +1,22 @@
+Name: Junhua Lai
+
+Programming Language: Java
+
+IDE / Editor: IntelliJ IDEA
+
+## Question 1 - What does ADT stand for?
+ADT stands for abstract data types.
+
+## Question 2 - In your own words, what is an Abstract Data Type?
+Abstract Data Type describes what a data type does.
+
+## Question 3 - What is the difference between an ADT and its implementation?
+### Use the following idea in your explanation: WHAT versus: HOW
+Abstract Data Types describe what the data type does while the implementation is how it is implemented in code.
+
+## Question 4 - Can two programmers create different implementations of the same ADT? Explain your answer.
+Yes, ADT only describe what something does, programmers can implement it in different ways as long as it follows 
+behavior described by the ADT. For example, one can use array and another can use a linked list to get to the same outcome.
+
+## Question 5 - If one programmer creates a Stack using an array and another creates a Stack using a linked list, are both still Stacks? Explain why.
+Yes, if the output of the code follows the behavior of what the stack ADT described, then it's just different implementations.
